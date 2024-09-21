08ff0e0b3627df53cfc9e3d5d355f078413421e1
bramirez96 <bran.ramirez.don@gmail.com>
2024-09-21 03:15:04 -0700

    New export pattern

--- src/Contentful.ts
+++
@@ -1,6 +0,0 @@
-// ! Copyright (c) 2024, Brandon Ramirez, brr.dev
-
-export * from "./api";
-export * from "./types";
-export * from "./ContentfulAPI.types";
-export * from "./classes";
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@
  * @copyright Copyright (c) 2024
  */
 
-export * as Contentful from './Contentful';
+export * from './classes';
+export * from './ContentfulAPI.types';
